03b528d6165044d971fa5a62f70878a701471f48
shubhsriv2018 <shubh.sriv2018@gmail.com>
2026-06-12 06:02:27 +0000

    push

--- mysite_cpy/mycrm/views.py
+++ mysite_cpy/mycrm/views.py
@@ -1,7 +1,19 @@
+#from django.shortcuts import render
+#
+#def index(request):
+#    return render(request, 'core/index.html')
 from django.shortcuts import render
+from .models import Customer
 
 def index(request):
-    return render(request, 'core/index.html')
+    customers = Customer.objects.all()
 
+    return render(
+        request,
+        'mycrm/index.html',
+        {
+            'customers': customers
+        }
+    )
 #def about(request):
 #    return render(request, 'core/about.html')
