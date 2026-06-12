7cbc50452e3f6631e6ac3d28b399a5eabe823ea7
shubhsriv2018 <shubh.sriv2018@gmail.com>
2026-06-12 05:32:04 +0000

    push

--- mysite/mycrm/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    return render(request, 'core/index.html')
-
-#def about(request):
-#    return render(request, 'core/about.html')
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -21,5 +21,4 @@ from mycrm.views import index
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', index, name='index'),
 ]
