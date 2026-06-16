a0d166df5455284a735827f4b4401008e5f43083
shubhsriv2018 <shubh.sriv2018@gmail.com>
2026-06-16 04:15:22 +0000

    login

--- mysite_cpy/mycrm/views.py
+++ mysite_cpy/mycrm/views.py
@@ -2,9 +2,33 @@
 #
 #def index(request):
 #    return render(request, 'core/index.html')
-from django.shortcuts import render
+#from django.shortcuts import render
 from .models import Customer
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.error(request, 'Invalid username or password')
+
+    return render(request, 'mycrm/login.html')
+
+@login_required   
 def index(request):
     customers = Customer.objects.all()
 
--- mysite_cpy/mysite/settings.py
+++ mysite_cpy/mysite/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+LOGIN_URL = '/login/'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
@@ -26,7 +26,6 @@ SECRET_KEY = "django-insecure-$q_x_a1h+lvhp5o9#s&1b#45r#1-5qgzpl3vrcxqa7-hxotowc
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
